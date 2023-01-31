330c372fc23b519f2524b96cb90104563e779743
dineshkarthi12 <120086241+dineshkarthi12@users.noreply.github.com>
2023-01-31 17:19:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Typed from "react-typed";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="title">React Typing Animation Effect</h1>{" "}
+      </div>
+      <div className="animated-typing">
+        <Typed
+          strings={[
+            "I'm a Full Stack Developer",
+            "I Love Software Development",
+            "I Love All My Subscribers",
+          ]}
+          typeSpeed={150}
+          backSpeed={100}
+          loop
+        />
+      </div>
+      <div className="buttons">
+        <button className="btn-pink">Learn More</button>
+        <button className="btn-purple">Talk To Us Now</button>
+      </div>
+    </div>
+  );
+}
+export default App;
+
